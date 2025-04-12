82712bcf9b5ef098a33419001c65aa8325a1df29
Filip Oliver Klimoszek <filiver123@gmail.com>
2025-04-12 09:48:36 +0200

    New organize

--- organize_new.py
+++ organize_new.py
@@ -4,6 +4,12 @@ from pydicom.errors import InvalidDicomError
 from collections import defaultdict
 from datetime import datetime
 
+def add_frame_of_reference(output, frame_of_reference, modality, file):
+    if frame_of_reference not in output:
+        output[frame_of_reference] = {}
+    if modality not in output[frame_of_reference]:
+        output[frame_of_reference][modality] = []
+    output[frame_of_reference][modality].append(file)
 
 def process_sample(sample_dir):
     output = {}
@@ -14,19 +20,22 @@ def process_sample(sample_dir):
         elif item.lower().endswith(".dcm"):
             try:
                 ds = pydicom.dcmread(item_path, stop_before_pixels=True)
-                # print(f"Processing DICOM file: {item_path}")
-                # Add your processing logic here
-                if ds.FrameOfReferenceUID not in output:
-                    output[ds.FrameOfReferenceUID] = {}
-                if ds.Modality not in output[ds.FrameOfReferenceUID]:
-                    output[ds.FrameOfReferenceUID][ds.Modality] = []
-                output[ds.FrameOfReferenceUID][ds.Modality].append(item)
+                if ds.Modality == "RTSTRUCT":
+                    for rfors in ds.ReferencedFrameOfReferenceSequence:
+                        frame_of_reference = rfors.FrameOfReferenceUID
+                        add_frame_of_reference(output, frame_of_reference, "RTSTRUCT", item)
+                else:
+                    add_frame_of_reference(output, ds.FrameOfReferenceUID, ds.Modality, item)
+                    # if ds.FrameOfReferenceUID not in output:
+                    #     output[ds.FrameOfReferenceUID] =  {}
+                    # if ds.Modality not in output[ds.FrameOfReferenceUID]:
+                    #     output[ds.FrameOfReferenceUID][ds.Modality] = []
+                    # output[ds.FrameOfReferenceUID][ds.Modality].append(item)
             except InvalidDicomError:
                 print(f"Invalid DICOM file: {item_path}")
             except Exception as e:
                 print(f"Error processing {item_path}: {e}")
     return output
-
 
 def process_all_samples(main_dir):
     """
@@ -48,7 +57,7 @@ def process_all_samples(main_dir):
     for item in os.listdir(main_dir):
         item_path = os.path.join(main_dir, item)
         if os.path.isdir(item_path):
-            output[item] = process_sample(item_path)
+            output[item] =  process_sample(item_path)
 
     print("Processing finished.")
     return output
@@ -74,7 +83,6 @@ if __name__ == "__main__":
             for identifier in output[sample]:
                 counts[sample][identifier] = {}
                 for modality in output[sample][identifier]:
-                    counts[sample][identifier][modality] = len(
-                        output[sample][identifier][modality])
+                    counts[sample][identifier][modality] = len(output[sample][identifier][modality])
 
         print(counts)
